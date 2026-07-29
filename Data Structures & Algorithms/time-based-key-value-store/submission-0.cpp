e58a47a945d98064ae2b47179cd48fed7da1218b
Sneha-452 <sdsd952800@gmail.com>
2026-07-29 23:43:38 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,39 @@
+class TimeMap {
+public:
+   unordered_map<string, vector<pair<int, string>>> mp;
+
+    TimeMap() {
+
+    }
+
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+
+        if (mp.find(key) == mp.end())
+            return "";
+
+        auto &v = mp[key];
+
+        int left = 0;
+        int right = v.size() - 1;
+
+        string ans = "";
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (v[mid].first <= timestamp) {
+                ans = v[mid].second;
+                left = mid + 1;      // larger valid timestamp dhundo
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+        
+    }
+};
